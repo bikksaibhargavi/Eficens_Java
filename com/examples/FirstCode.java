84abf23d031d235203d628ec84117fbbce86ad4a
bikki <bikkisaibhargavi@gmail.com>
2023-06-03 13:12:35 -0500

    My second Program

--- com/examples/FirstCode.java
+++ com/examples/FirstCode.java
@@ -1,7 +1,11 @@
 package com.examples;
 
+import oops.Vehicle;
+
 public class FirstCode {
     public static void main (String[] args){
-        System.out.println("My First Program");
+        Vehicle car = new Vehicle();
+        car.start();
+        car.stop();
     }
 }
---
+++ oops/Vehicle.java
@@ -0,0 +1,13 @@
+package oops;
+
+public class Vehicle {
+    public void start()
+    {
+        System.out.println("Vehicle Start");
+    }
+    public void stop()
+    {
+        System.out.println("Vehicle stop");
+    }
+
+}
